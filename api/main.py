e33f26ba8585a3a1a841fe013f1c8a475431475c
Jak <yusaliano@gmail.com>
2025-02-26 21:35:55 +0700

    add __init__.py and change forms to .forms

--- api/main.py
+++ api/main.py
@@ -9,7 +9,7 @@ from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Text
 from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
-from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
+from .forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 import os
 # Optional: add contact me email functionality (Day 60)
 # import smtplib
